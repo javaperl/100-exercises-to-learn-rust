0ff017039daf5c1f9413274de6f5cf0b998adb7f
Kevin Wang <javaperl@gmail.com>
2025-04-24 18:59:39 -0700

    02 06 finished

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut m = n;
+    let mut r: u32 = 1;
+
+    while m > 1 {
+        r *= m;
+        m -= 1;
+    }
+
+    r
 }
 
 #[cfg(test)]
