6b95c6cb996f1cb3186d160f58547684216fd939
Kevin Wang <javaperl@gmail.com>
2025-04-24 19:13:55 -0700

    02 07 finished

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut r: u32 = 1;
+
+    for i in 1..(n + 1) {
+        r *= i;
+    }
+
+    r
 }
 
 #[cfg(test)]
